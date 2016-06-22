39c39649f7781a3d28c255b77294b57cea523c06
belaustegui <cristian.alvb@gmail.com>
2016-06-22 15:53:21 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var GoogleSpreadsheet = require('google-spreadsheet'),
-    async = require('async');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
 
 async.waterfall([
   loadDocument,
@@ -7,14 +7,14 @@ async.waterfall([
   getProjectId,
   getResources,
   getResourceDepartments,
-  gettResourceDedications,
+  getResourceDedications,
 ], function (err, document) {
   console.log(JSON.stringify(document));
 });
 
 function loadDocument(callback) {
-  var doc = new GoogleSpreadsheet('1H6oKo71NOapisLnjHltXbDlyLfp28U4Bni5SHyt1u48'),
-      creds = require('./credentials.json');
+  var doc = new GoogleSpreadsheet('1H6oKo71NOapisLnjHltXbDlyLfp28U4Bni5SHyt1u48');
+  var creds = require('./credentials.json');
   doc.useServiceAccountAuth(creds, function (err) {
     callback(err, doc);
   });
@@ -59,24 +59,18 @@ function getResourceDepartments(doc, project, callback) {
   });
 }
 
-function gettResourceDedications(doc, project, callback) {
+function getResourceDedications(doc, project, callback) {
   var weekCells = {'min-row': 15, 'min-col': 1, 'max-col': 1};
   doc.getCells(1, weekCells, function (err, cells) {
     // We store an array of every registered week converted into a Date object.
     // Values that are not dates will be stored as null.
-    var weeks = cells.map(function (cell) {
-          var parsed = cell.value.split("/").map(function (x) { return parseInt(x); });
-          // Dates come in a format MM/DD/YYYY
-          if (parsed.length !== 3) { return null; }
-          // JS requires months to start in 0, not 1.
-          return new Date(Date.UTC(parseInt(parsed[2]), parseInt(parsed[0]) - 1, parseInt(parsed[1])));
-        }),
-        dedicationCells = {'min-row': 15, 'max-row': 15 + weeks.length - 1, 'min-col': 3, 'max-col': 3 + project.resources.length - 1, 'return-empty': true};
+    var weeks = cells.map(_parseWeek);
+    var dedicationCells = {'min-row': 15, 'max-row': 15 + weeks.length - 1, 'min-col': 3, 'max-col': 3 + project.resources.length - 1, 'return-empty': true};
     // Once we have registered all weeks, we go for the dedications.
     doc.getCells(1, dedicationCells, function (err, cells) {
       project.resources = project.resources.map(function (resource, index) {
-        var resourceCol = 3 + index,
-            resourceHours = cells.filter(function (cell) { return cell.col == resourceCol; });
+        var resourceCol = 3 + index;
+        var resourceHours = cells.filter(function (cell) { return cell.col == resourceCol; });
         // For each resource we get its dedicated hours, which are stored in an array
         // so the resourceHours[X] represents the hours dedicated in the week[X]
         resource.dedications = weeks.map(function (week, index) { return {week: week, dedication: resourceHours[index].value}; })
@@ -86,4 +80,15 @@ function gettResourceDedications(doc, project, callback) {
       callback(null, project);
     });
   });
+}
+
+function _parseWeek(cell) {
+  var components = cell.value.split("/").map(function (x) { return parseInt(x); });
+  // Dates come in a format MM/DD/YYYY
+  if (components.length === 3) {
+    // JS requires months to start in 0, not 1.
+    return new Date(Date.UTC(components[2], components[0] - 1, components[1]));
+  } else {
+    return null;
+  }
 }
