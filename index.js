f7d9b7d25b09972579a87da52972100e8dd3cebf
belaustegui <cristian.alvb@gmail.com>
2016-06-22 13:12:11 +0200

    Remove the document as a global variable

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var GoogleSpreadsheet = require('google-spreadsheet'),
-    async = require('async'),
-    doc = new GoogleSpreadsheet('1H6oKo71NOapisLnjHltXbDlyLfp28U4Bni5SHyt1u48');
+    async = require('async');
 
 async.waterfall([
-  setAuth,
+  loadDocument,
   getProjectName,
   getProjectId,
   getResources,
@@ -12,41 +11,43 @@ async.waterfall([
   console.log(document);
 });
 
-// Authenticate with Google Drive
-function setAuth(callback) {
-  var creds = require('./credentials.json');
-  doc.useServiceAccountAuth(creds, callback);
+function loadDocument(callback) {
+  var doc = new GoogleSpreadsheet('1H6oKo71NOapisLnjHltXbDlyLfp28U4Bni5SHyt1u48'),
+      creds = require('./credentials.json');
+  doc.useServiceAccountAuth(creds, function (err) {
+    callback(err, doc);
+  });
 }
 
-function getProjectName(callback) {
+function getProjectName(doc, callback) {
   var projectNameCell = {'min-row': 2, 'max-row': 2, 'min-col': 3, 'max-col': 3};
   doc.getCells(1, projectNameCell, function (err, cells) {
     // We only expect a single cell.
     var project = {name: cells[0].value};
-    callback(null, project);
+    callback(null, doc, project);
   });
 }
 
-function getProjectId(project, callback) {
+function getProjectId(doc, project, callback) {
   var projectIdCell = {'min-row': 1, 'max-row': 1, 'min-col': 4, 'max-col': 4};
   doc.getCells(1, projectIdCell, function (err, cells) {
     // We only expect a single cell.
     project.code = cells[0].value;
-    callback(null, project);
+    callback(null, doc, project);
   });
 }
 
-function getResources(project, callback) {
+function getResources(doc, project, callback) {
   var resourceCells = {'min-row': 5, 'max-row': 5, 'return-empty': false};
   doc.getCells(1, resourceCells, function (err, cells) {
     project.resources = cells.map(function (cell) {
       return {name: cell.value.replace("\n", " ")};
     });
-    callback(null, project);
+    callback(null, doc, project);
   });
 }
 
-function getResourceDepartments(project, callback) {
+function getResourceDepartments(doc, project, callback) {
   var departmentCells = {
     'min-row': 3,
     'max-row': 3,
@@ -55,15 +56,14 @@ function getResourceDepartments(project, callback) {
     'return-empty': true
   };
   doc.getCells(1, departmentCells, function (err, cells) {
-    console.log(cells);
     var departments = cells.map(function (cell) { return cell.value; });
     departments.forEach(function (department, index) {
       project.resources[index].department = department;
     });
-    callback(null, project);
+    callback(null, doc, project);
   });
 }
 
-function getResourceDedications(project, callback) {
-
+function getResourceDedications(doc, project, callback) {
+  // TODO fill with code
 }
